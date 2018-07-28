f8beaecc6f065124269f8cc0e0128532fecb8f8f
Karen Kraft <kraftkj@comcast.net>
2018-07-28 16:46:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # dog.rb
+class Dogs
+  def initialize(name)
+    @name = name 
+  end
+  
+  def return_name
+    @name
+  end 
+  
+  def bark
+    puts "#{@name} says Woof!"
+  end 
+  
+  def sit 
+    puts "Fido, sit!"
+  end 
+end
+fido = Dogs.new("Fido")
+snoopie = Dogs.new("Snoopie")
+lassie = Dogs.new("Lassie")
+
+puts fido.return_name
+fido.bark
+
+puts fido.return_name
+fido.sit 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # person.rb
+class Person 
+  def initialize(person)
+    @person = person
+  end 
+  def return_person 
+    @person 
+  end
+  def talks
+    puts "#{@person} says Hello World"
+end 
+def walks
+    puts "#{@person} is walking"
+end 
+end
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_kay = Person.new("Alan Kay")
+ 
+ puts adele_goldberg.return_person 
+ adele_goldberg.talks
+ adele_goldberg.walks
+    
+    
+  
